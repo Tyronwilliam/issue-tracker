80d46bb40a39e35865b565007c95d7c3911523c9
Tyronwilliam <tyronwillianchanu@gmail.com>
2023-10-11 14:17:21 +0200

    Add loading skeletons

---
+++ app/components/IssueStatusBadge.tsx
@@ -0,0 +1,22 @@
+import { Status } from "@prisma/client";
+import { Badge } from "@radix-ui/themes";
+import React from "react";
+
+const statusMap: Record<
+  Status,
+  { label: string; color: "red" | "violet" | "green" }
+> = {
+  OPEN: { label: "Ouvert", color: "red" },
+  IN_PROGRESS: { label: "En cours", color: "violet" },
+  CLOSED: { label: "Fait", color: "green" },
+};
+
+const IssueStatusBadge = ({ status }: { status: Status }) => {
+  return (
+    <Badge color={statusMap[status].color} >
+      {statusMap[status].label}
+    </Badge>
+  );
+};
+
+export default IssueStatusBadge;
