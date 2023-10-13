7f46be7bd47815dfa5680bd3f239431e3110c811
Tyronwilliam <tyronwillianchanu@gmail.com>
2023-10-13 09:38:59 +0200

    Show confirmation dialog delete

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,7 +1,34 @@
-import { Button } from "@radix-ui/themes";
+"use client";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
-  return <Button color="red">DeleteIssueButton</Button>;
+  return (
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">Supprimer la tâche</Button>
+      </AlertDialog.Trigger>{" "}
+      <AlertDialog.Content style={{ maxWidth: 450 }}>
+        <AlertDialog.Title>Confirmer la suppréssion</AlertDialog.Title>
+        <AlertDialog.Description size="2">
+          Êtes-vous sûr ? Cette action est irréversible et la tâche sera
+          définitivement supprimé.
+        </AlertDialog.Description>
+
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Annuler
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="solid" color="red">
+              Supprimer la tâche
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  );
 };
 
 export default DeleteIssueButton;
