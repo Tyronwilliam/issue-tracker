dc447feaffb21ee438602c24faefa7cb2b05d74c
Tyronwilliam <tyronwillianchanu@gmail.com>
2023-10-15 11:42:33 +0200

    Populate assign components

--- app/issues/[id]/AssignSelect.tsx
+++ app/issues/[id]/AssignSelect.tsx
@@ -1,19 +1,31 @@
 "use client";
+import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const AssignSelect = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const { data } = await axios.get<User[]>("/api/users");
+      setUsers(data);
+    };
+    fetchUser();
+  }, []);
   return (
     <Select.Root defaultValue="">
       <Select.Trigger />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestion</Select.Label>
-          <Select.Item value="orange">Orange</Select.Item>
-          <Select.Item value="apple">Apple</Select.Item>
-          <Select.Item value="grape" disabled>
-            Grape
-          </Select.Item>
+          {users.length > 0 &&
+            users.map((user) => (
+              <Select.Item value={user.id} key={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
