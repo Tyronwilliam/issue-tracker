8ff163f3e034d02520eae1dedbf83bbe93ba4bb0
Tyronwilliam <tyronwillianchanu@gmail.com>
2023-12-04 12:21:20 +0100

    handle errors submit categorie + refresh

--- app/api/categorie/[id]/route.ts
+++ app/api/categorie/[id]/route.ts
@@ -2,11 +2,33 @@ import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import prisma from "@/prisma/client";
 import { authOptions } from "../../auth/[...nextauth]/route";
+import { patchCategorieSchema } from "@/app/validationSchema";
 
-export async function GET(request: NextRequest) {
+export async function POST(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
   const session = await getServerSession(authOptions);
   if (!session) return NextResponse.json({}, { status: 401 });
+  
+  const body = await request.json();
+  const validationCategorie = patchCategorieSchema.safeParse(body);
 
-  const categorie = await prisma.categorieCustom.findMany();
+  if (!validationCategorie.success) {
+    return NextResponse.json(validationCategorie.error.format(), {
+      status: 400,
+    });
+  }
+
+  const { title, hexCode } = body;
+  const categorie = await prisma.categorieCustom.create({
+    data: {
+      title,
+      hexCode,
+      issue: {
+        connect: { id: parseInt(params?.id) },
+      },
+    },
+  });
   return NextResponse.json(categorie, { status: 200 });
 }
--- app/issues/_components/CustomCategorie.tsx
+++ app/issues/_components/CustomCategorie.tsx
@@ -1,8 +1,10 @@
 import { Spinner } from "@/app/components";
 import { CategorieCustom } from "@prisma/client";
+import { InfoCircledIcon } from "@radix-ui/react-icons";
 import {
   Box,
   Button,
+  Callout,
   Dialog,
   Flex,
   Select,
@@ -10,7 +12,8 @@ import {
   TextField,
 } from "@radix-ui/themes";
 import axios from "axios";
-import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useRef, useState } from "react";
 import { CirclePicker, Color, ColorResult } from "react-color";
 import toast from "react-hot-toast";
 
@@ -19,42 +22,61 @@ interface CategorieProps {
   categorie?: CategorieCustom;
   issueId: number;
 }
-
+interface Error {
+  title: string;
+  hexCode: string;
+}
 const CustomCategorie = ({
   allCategorie,
   categorie,
   issueId,
 }: CategorieProps) => {
+  const router = useRouter();
+  const title = useRef<HTMLInputElement>(null);
   const [color, setColor] = useState("");
-  const [error, setError] = useState("");
+  const [error, setError] = useState<Error>({
+    title: "",
+    hexCode: "",
+  });
   const [open, setOpen] = useState(false);
   const [isSubmitting, setIsSubmitting] = useState(false);
   const handleChangeComplete = (color: ColorResult) => {
     const newColor = color.hex;
-    console.log(newColor, "NEX COLOR");
     setColor(newColor);
   };
 
-  const handleSubmit = async () => {
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
     setIsSubmitting(true);
-    // setOpen(false);
     let data = {
-      categorie: {
-        title: "",
-        hexCode: "",
-      },
+      title: title.current?.value,
+      hexCode: color,
     };
-    // const response = await axios.patch("/api/categorie/" + issueId, data);
 
-    // if (response?.status === 200) {
-    //   setIsSubmitting(false);
-
-    //   toast.success("C'est fait ! ");
-    // } else {
-    //   setIsSubmitting(false);
-
-    //   toast.error("Oups ! Modification impossible");
-    // }
+    const response = await axios
+      .post("/api/categorie/" + issueId, data)
+      .then((res) => res)
+      .catch((err) => err);
+    console.log(response, "Response from POST ");
+    if (response?.status === 200) {
+      setIsSubmitting(false);
+      setOpen(false);
+      toast.success("C'est fait ! ");
+      router.refresh();
+    } else {
+      let errors = { title: "", hexCode: "" };
+      const titleError = response?.response?.data?.title;
+      const hexCodeError = response?.response?.data?.hexCode;
+      if (titleError) {
+        errors.title = response?.response?.data?.title?._errors[0];
+      }
+      if (hexCodeError) {
+        errors.hexCode = response?.response?.data?.hexCode?._errors[0];
+      }
+      setIsSubmitting(false);
+      setError(errors);
+      toast.error("Oups ! Modification impossible");
+    }
   };
   return (
     <Dialog.Root open={open} onOpenChange={() => setOpen(true)}>
@@ -88,6 +110,22 @@ const CustomCategorie = ({
             </Select.Content>
           </Select.Root>
           <Text as="span">OU</Text>
+          {(error.title || error.hexCode) && (
+            <Callout.Root>
+              <Callout.Icon>
+                <InfoCircledIcon />
+              </Callout.Icon>
+              <Callout.Text>{error.title || error.hexCode}</Callout.Text>
+            </Callout.Root>
+          )}
+          {error?.hexCode && (
+            <Callout.Root>
+              <Callout.Icon>
+                <InfoCircledIcon />
+              </Callout.Icon>
+              <Callout.Text>{error?.hexCode}</Callout.Text>
+            </Callout.Root>
+          )}
           <form onSubmit={handleSubmit}>
             <Flex gap="3" direction={"column"} className="w-fit">
               <TextField.Root className="pr-2">
@@ -97,6 +135,7 @@ const CustomCategorie = ({
                   placeholder="Créez votre catégorie personnalisée"
                   aria-label="Nom de catégorie"
                   aria-required="true"
+                  ref={title}
                 />
               </TextField.Root>
               <Box>
