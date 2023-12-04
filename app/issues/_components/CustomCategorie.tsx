61be927be862fde850e289d8ab8599c4b599200f
Tyronwilliam <tyronwillianchanu@gmail.com>
2023-12-04 10:56:04 +0100

    add color picker

--- app/issues/_components/CustomCategorie.tsx
+++ app/issues/_components/CustomCategorie.tsx
@@ -7,12 +7,22 @@ import {
   Text,
   TextField,
 } from "@radix-ui/themes";
+import { useState } from "react";
+import { CirclePicker, Color, ColorResult } from "react-color";
 
 interface CategorieProps {
   allCategorie: CategorieCustom[];
   categorie?: CategorieCustom;
 }
+
 const CustomCategorie = ({ allCategorie, categorie }: CategorieProps) => {
+  const [color, setColor] = useState("");
+
+  const handleChangeComplete = (color: ColorResult) => {
+    const newColor = color.hex;
+    console.log(newColor, "NEX COLOR");
+    setColor(newColor);
+  };
   return (
     <Dialog.Root>
       <Dialog.Trigger>
@@ -49,6 +59,10 @@ const CustomCategorie = ({ allCategorie, categorie }: CategorieProps) => {
             <TextField.Root>
               <TextField.Input placeholder="Créez votre catégorie personnalisée" />
             </TextField.Root>
+            <MyColorPickerComponent
+              color={color}
+              onChangeComplete={handleChangeComplete}
+            />
           </form>
         </Flex>
         {/*  */}
@@ -68,3 +82,15 @@ const CustomCategorie = ({ allCategorie, categorie }: CategorieProps) => {
 };
 
 export default CustomCategorie;
+
+type MyColorPickerComponentProps = {
+  onChangeComplete: (ColorResult: ColorResult) => void;
+  color: Color | undefined;
+};
+
+const MyColorPickerComponent = ({
+  color = "#f44336",
+  onChangeComplete,
+}: MyColorPickerComponentProps) => {
+  return <CirclePicker color={color} onChangeComplete={onChangeComplete} />;
+};
