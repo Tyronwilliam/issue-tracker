044334dd0e962cdf26c23e983e410b56d74ed2a4
Tyronwilliam <tyronwillianchanu@gmail.com>
2023-12-06 11:39:25 +0100

    Refactor

--- app/issues/list/IssueCells.tsx
+++ app/issues/list/IssueCells.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { IssueStatusBadge, Link } from "@/app/components";
+import { IssueStatusBadge } from "@/app/components";
 import { useIssueContext } from "@/app/hooks/useIssueContext";
 import { useTimerContext } from "@/app/hooks/useTimerContext";
 import useToggle from "@/app/hooks/useToggle";
@@ -7,20 +7,16 @@ import { Time, options } from "@/app/utils/service/timeFunction";
 import { CategorieCustom, Issue } from "@prisma/client";
 import { Flex, Table } from "@radix-ui/themes";
 import AssignStatus from "../[id]/AssignStatus";
-import { IconeTimer } from "./IconeTimer";
 import CustomCategorie from "../_components/CategorieComponent/CustomCategorie";
-import { IssueWithProjectAndCategory } from "./page";
+import { IconeTimer } from "./IconeTimer";
 import QuickEditTitle from "./QuickEditTitle";
-import { useRef, useState } from "react";
-import axios from "axios";
-import { useRouter } from "next/navigation";
-import toast from "react-hot-toast";
+import { IssueWithProjectAndCategory } from "./page";
+import useQuickEditTitle from "./hook/useQuickEditTitle";
 interface Props {
   issues: IssueWithProjectAndCategory[];
   allCategorie: CategorieCustom[];
 }
 const IssueCells = ({ issues, allCategorie }: Props) => {
-  const router = useRouter();
   const {
     setShowToast,
     setCurrentTimer,
@@ -28,42 +24,17 @@ const IssueCells = ({ issues, allCategorie }: Props) => {
     createTimer,
     timerRunning,
   } = useTimerContext();
+  const {
+    toggleQuickEdit,
+    handlePressKey,
+    handlePatchIsue,
+    openQuickEdit,
+    selectId,
+    title,
+  } = useQuickEditTitle();
   const { open, toggle, itemId } = useToggle();
   const { issueTime, handleTimeChange, setIssueTime } = useIssueContext();
-  const title = useRef<HTMLInputElement>(null);
-  const [openQuickEdit, setOpenQuickEdit] = useState(false);
-  const [selectId, setSelectId] = useState<number | null>(null);
-  const toggleQuickEdit = (id: number) => {
-    setOpenQuickEdit(!openQuickEdit);
-    setSelectId(id);
-  };
 
-  const handlePatchIsue = async (issueId: number) => {
-    let data = {
-      title: title.current?.value,
-    };
-    const response = await axios
-      .patch("/api/issues/" + issueId, data)
-      .then((res) => res)
-      .catch((err) => err);
-    if (response?.status === 200) {
-      toast.success("C'est fait !");
-
-      setOpenQuickEdit(!openQuickEdit);
-      router.refresh();
-    } else {
-      const error = response?.response?.data?.title?._errors[0];
-      toast.error(error);
-    }
-  };
-  const handlePressKey = async (
-    e: React.KeyboardEvent<HTMLInputElement>,
-    issueId: number
-  ) => {
-    if (e.key === "Enter") {
-      handlePatchIsue(issueId);
-    }
-  };
   return issues?.map((issue) => {
     const date = new Date(issue?.createdAt);
     const formatDate = date.toLocaleDateString(undefined, options);
